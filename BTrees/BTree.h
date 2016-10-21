312ae5edf8c7c6e47a95f84f10a0c98f926039bf
Bjorn Vandenbussche <bjorn.vandenbussche@delawareconsulting.com>
2016-10-21 16:45:34 +0200

    Getting there on BTrees

--- BTrees/BTree.h
+++ BTrees/BTree.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <vector>
 #include "Disk.h"
 #include <stack>
 //betekenis m: orde van de B-tree (maximaal m kinderen en m-1 sleutels)
@@ -10,6 +9,14 @@ template <class T, class D, unsigned int m>
 class BTree;
 template <class T, class D, unsigned int m>
 class BNode;
+
+struct DataLocation
+{
+	blockindex blockindex_ = 0;
+	int keyindex = 0;
+	bool found = false;
+	stack<blockindex> path;
+};
 
 template <class T, class D, unsigned int m>
 class BTree
@@ -25,30 +32,127 @@ public:
 	}
 
 	void Put(T key, D val);
-	void Search(T key);
+	DataLocation Search(T key);
 private:
-	Disk<BNode<T,D,m>>& disk;
-	Node<T,D,m> root;
+	Disk<BNode<T, D, m>>& disk;
+	Node root;
 	blockindex rootIndex;
-	D Search(T key, stack<blockindex>& path);
 };
 
 template <class T, class D, unsigned m>
 void BTree<T, D, m>::Put(T key, D val)
 {
-	//TODO
+	DataLocation location = Search(key);
+	if (location.found)
+	{
+		//exists already
+		return;
+	}
+	stack<blockindex> path = location.path;
+	bool added = false;
+	blockindex index;
+	blockindex newblockindex = 0;
+	while (!added && !path.empty())
+	{
+		index = path.top();
+		path.pop();
+		Node node;
+		disk.Read(node, index);
+
+		if (node.k < m) //still some space in current node
+		{
+			node.Insert(key, val, newblockindex);
+			disk.Rewrite(node, index);
+			added = true;
+		}
+		else // new brother with half of the values of the current node and shift m/2 up
+		{
+			Node brother;
+			unsigned int middle = m / 2;
+			T middleKey = node.keys[middle];
+			D middleValue = node.data[middle];
+
+			brother.k = node.k - middle - 1;
+			for (auto i = middle + 1; i < node.k; i++)
+			{
+				brother.keys[i-middle - 1] = node.keys[i];
+				brother.data[i - middle - 1] = node.data[i];
+			}
+			if(!node.isLeaf)
+			{
+				for (unsigned int i = middle + 1; i <= node.k; i++) {
+					brother.index[i - middle - 1] = node.index[i];
+				}
+			}
+			node.k = middle;
+			if(key < node.keys[middle])
+			{
+				node.Insert(key, val, newblockindex);
+			}
+			else {
+				brother.Insert(key, val, newblockindex);
+			}
+			key = middleKey;
+			val = middleValue;
+
+			disk.Rewrite(node, index);
+			newblockindex = disk.Write(brother);
+		}
+	}
+	//if we arrive at top but have a middle value that needs to be pushed up, we create a new root
+	if(!added && path.empty())
+	{
+		Node root;
+		root.isLeaf = false;
+		root.keys[0] = key;
+		root.data[0] = val;
+		root.index[0] = index;
+		root.index[1] = newblockindex;
+		rootIndex = disk.Write(root);
+	}
 }
 
 template <class T, class D, unsigned m>
-void BTree<T, D, m>::Search(T key)
+DataLocation BTree<T, D, m>::Search(T key)
 {
-	//TODO
-}
+	Node node;
+	blockindex index = rootIndex;
+	DataLocation location;
+	location.found = false;
+	stack<blockindex> path;
+	disk.Read(node, index);
+	path.push(index);
 
-template <class T, class D, unsigned m>
-D BTree<T, D, m>::Search(T key, stack<blockindex>& path)
-{
-	return {};
+	while (true)
+	{
+		unsigned int i = 0;
+		while (i < node.k && node.keys[i] < key)
+		{
+			i++;
+		}
+		if (i < node.k)
+		{
+			if (key == node.keys[i])
+			{
+				location.blockindex_ = index;
+				location.keyindex = i;
+				location.found = true;
+				location.path = path;
+				return location;
+			}
+			if (node.isLeaf)
+			{
+				return location;
+			}
+			index = node.index[i - 1];
+			path.push(index);
+			disk.Read(node, index);
+		}
+		else
+		{
+			return location;
+		}
+	}
 }
 
 template <class T, class D, unsigned int m>
@@ -57,13 +161,67 @@ class BNode
 private:
 	friend class BTree<T, D, m>;
 public:
-	vector<T> keys;
-	vector<D> data;	
+	BNode(): k(0), isLeaf(true)
+	{
+	}
 
+	const BNode<T, D, m>& operator=(const BNode<T, D, m>& b);
+
+	int Insert(T key, D val, blockindex index);
+
+	T keys[m - 1];
+	D data[m];
+	blockindex index[m + 1];
 	/// <summary>
 	/// The current number of keys
 	/// </summary>
 	unsigned int k;
-
 	bool isLeaf;
 };
+
+template <class T, class D, unsigned m>
+const BNode<T, D, m>& BNode<T, D, m>::operator=(const BNode<T, D, m>& b)
+{
+	k = b.k;
+	isLeaf = b.isLeaf;
+	for (unsigned int i = 0; i < k; i++)
+	{
+		keys[i] = b.keys[i];
+		data[i] = b.data[i];
+	}
+	if (!isLeaf)
+	{
+		for (unsigned int i = 0; i <= k; i++)
+		{
+			index[i] = b.index[i];
+		}
+	}
+	return *this;
+}
+
+template <class T, class D, unsigned m>
+int BNode<T, D, m>::Insert(T key, D val, blockindex bindex)
+{
+	unsigned int j = 0;
+	while (j < k && keys[j] < key) {
+		j++;
+	}
+
+	// Schuif alles erachter 1 plaats op.
+	for (unsigned int v = k; v > j; v--) {
+		keys[v] = keys[v - 1];
+		data[v] = data[v - 1];
+		if (!isLeaf) {
+			index[v + 1] = index[v];
+		}
+	}
+
+	// Plaats de sl daar.
+	keys[j] = key;
+	data[j] = val;
+	if (!isLeaf && bindex != 0) {
+		index[j+1] = bindex;
+	}
+	k++;
+	return j;
+}
--- BTrees/Main.cpp
+++ BTrees/Main.cpp
@@ -1,12 +1,54 @@
 #include <iostream>
 #include "BTree.h"
 using namespace std;
+#include <fstream>
+#include <iostream>
+#include <string>
 
+using namespace std;
 
-int main()
-{
-	BTree<int, double, 666> tree;
-	
+// gebruikte sleutel, kan een klasse zijn maar struct werkt ook.
+struct Word {
+	string w;
+	// vergelijkingsoperator (kleiner dan)
+	bool operator<(const Word &t) const {
+		int o = w.compare(t.w);
+		return (o < 0) ? true : false;
+	}
+
+	bool operator==(const Word &t) const {
+		int o = w.compare(t.w);
+		return (o == 0);
+	}
+};
+
+int main() {
+
+	Disk<BNode<Word, int, 666> > disk;
+	BTree<Word, int, 666> tree(disk);
+
+	ifstream ifile;
+	ifile.open("donquijote.txt");
+	if (!ifile) {
+		cout << "Unable to open file";
+		exit(1); // terminate with error
+	}
+
+	string woord;
+	ifile >> woord;
+	while (!ifile.fail()) {
+		Word my_woord;
+		my_woord.w = woord;
+		DataLocation location = tree.Search(my_woord);
+		BNode<Word, int, 666> node;
+		disk.Read(node, location.blockindex_);
+		tree.Put(my_woord, node.data[location.keyindex]);
+		ifile >> woord;
+	}
+
+	ifile.close();
+
+	cout << "Done";
 	cin.get();
 	return 0;
 }
