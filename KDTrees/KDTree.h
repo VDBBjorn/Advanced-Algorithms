2a43e0d4c9c4743891d99ba026fae434ff593d6f
Bjorn Vandenbussche <bjorn.vandenbussche@delawareconsulting.com>
2016-10-30 14:50:37 +0100

    KD trees

--- KDTrees/KDTree.h
+++ KDTrees/KDTree.h
@@ -1,13 +1,18 @@
 #pragma once
 #include <cstdlib>
 #include <iostream>
-#include <queue>
 #include <memory>
 #include "Point.h"
 
 using namespace std;
-
+class KDTree;
 class Node;
+
+enum class Dimension
+{
+	x = 0,
+	y = 1
+};
 
 class KDTree : public unique_ptr<Node>
 {
@@ -21,51 +26,118 @@ public:
 	explicit KDTree(Point p);
 
 	KDTree* Search(Point);
-	void Add(Point);
+	KDTree* Add(Point);
 	int Depth() const;
-	void Write(ostream& os) const;
+	void Write(ostream& os);
 
 	friend ostream& operator<<(ostream& os, KDTree& b)
 	{
 		b.Write(os);
 		return os;
 	}
+
+protected:
+	KDTree* Search(Point, Node*&);
 };
 
 class Node
 {
+	friend class KDTree;
 public:
 	Node()
 	{
 	}
 
-	Node(Point k) : point(k)
+	Node(Point k) : point(k), dimension(Dimension::x)
 	{
 	}
 
-	friend class KDTree;
+	Node(Point k, Dimension d): point(k), dimension(d)
+	{
+	}
+
+	Dimension GetNextDimension() const;
 private:
 	Point point;
 	KDTree left, right;
+	Dimension dimension;
 };
 
-KDTree::KDTree(Point p) : unique_ptr<Node>(new Node(p))
+inline KDTree::KDTree(Point p) : unique_ptr<Node>(new Node(p))
 {
 }
 
 KDTree* KDTree::Search(Point k)
 {
-
+	Node* n = new Node();
+	return Search(k, n);
 }
 
-void KDTree::Add(Point k)
+inline Dimension Node::GetNextDimension() const
 {
+	if (this != nullptr && this->dimension == Dimension::x)
+	{
+		return Dimension::y;
+	}
+	return Dimension::x;
+}
+
+inline KDTree* KDTree::Add(Point k)
+{
+	Node* parent = nullptr;
+	KDTree* place = Search(k, parent);
+	if (place->get() == nullptr)
+	{
+		Node* node = new Node(k, parent->GetNextDimension());
+		*place = unique_ptr<Node>(node);
+	}
+	return place;
 }
 
 int KDTree::Depth() const
 {
+	return 0;
 }
 
-void KDTree::Write(ostream& os) const
+void KDTree::Write(ostream& os)
 {
+	if (this->get() != nullptr)
+	{
+		this->get()->left.Write(os);
+		os << this->get()->point << "\t" << static_cast<int>(this->get()->dimension) << endl;
+		this->get()->right.Write(os);
+	}
+}
+
+inline KDTree* KDTree::Search(Point p, Node*& parent)
+{
+	Dimension d = Dimension::x;
+	KDTree* tree = this;
+
+	while (tree->get() != nullptr && !(tree->get()->point == p)) {
+		parent = tree->get();
+		if (tree->get()->dimension == Dimension::x)
+		{
+			if (tree->get()->point.x < p.x)
+			{
+				tree = &(tree->get()->right);
+			}
+			else
+			{
+				tree = &(tree->get()->left);
+			}
+		}
+		else
+		{
+			if (tree->get()->point.y < p.y)
+			{
+				tree = &(tree->get()->right);
+			}
+			else
+			{
+				tree = &(tree->get()->left);
+			}
+		}
+	}
+	return tree;
 }
--- KDTrees/Main.cpp
+++ KDTrees/Main.cpp
@@ -1,7 +1,55 @@
 #include "KDTree.h"
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+void split(const std::string& s, char delim, std::vector<std::string>& elems)
+{
+	std::stringstream ss;
+	ss.str(s);
+	std::string item;
+	while (std::getline(ss, item, delim))
+	{
+		elems.push_back(item);
+	}
+}
+
+std::vector<std::string> split(const std::string& s, char delim)
+{
+	std::vector<std::string> elems;
+	split(s, delim, elems);
+	return elems;
+}
 
 int main()
 {
 	KDTree tree;
+	/*string line;
+	ifstream myfile("input.txt");
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			vector<string> elements = split(line, ' ');
+			Point p(stoi(elements[0]), stoi(elements[1]));
+			tree.Add(p);
+		}
+		myfile.close();
+	}
+	else cout << "Unable to open file";
 
+	cout << "done reading" << endl;*/
+
+	tree.Add(Point(3, 7));
+	tree.Add(Point(8, 1));
+	tree.Add(Point(6, 6));
+	tree.Add(Point(2, 6));
+	tree.Add(Point(1, 7));
+	tree.Add(Point(8, 6));
+	tree.Add(Point(5, 9));
+
+	cout << tree << endl;
+	cin.get();
+	return 0;
 }
