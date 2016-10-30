c40fe8a1f015e23d1ae37183e09e618bd8766027
Bjorn Vandenbussche <bjorn.vandenbussche@delawareconsulting.com>
2016-10-30 13:16:44 +0100

    QuadTree done

--- QuadTrees/Main.cpp
+++ QuadTrees/Main.cpp
@@ -16,6 +16,7 @@ int main() {
 	p.x = 20;
 	p.y = 40;
 	auto result = quadtree.Search(p);
+	cout << *result << endl;
 	cout << "Diepte: " << quadtree.Depth() << endl;
 	cout << quadtree << endl;
 	cin.get();
--- QuadTrees/QuadTree.h
+++ QuadTrees/QuadTree.h
@@ -8,65 +8,87 @@
 
 using namespace std;
 
-struct Point {
+struct Point
+{
 	int x;
 	int y;
 };
 
 class Node;
 
-class QuadTree : public unique_ptr<Node> {
+class QuadTree : public unique_ptr<Node>
+{
 public:
 	using unique_ptr<Node>::unique_ptr;
-	QuadTree() {};
+
+	QuadTree()
+	{
+	};
 	QuadTree(Point p);
 
 	QuadTree* Search(Point);
 	void Add(Point);
 	int Depth() const;
-	void Write(ostream & os) const;
+	void Write(ostream& os) const;
 
-	friend ostream& operator<< (ostream & os, QuadTree & b) {
+	friend ostream& operator<<(ostream& os, QuadTree& b)
+	{
 		b.Write(os);
 		return os;
 	}
 };
 
-class Node {
+class Node
+{
 public:
-	Node() {
-		for (int i = 0; i<4; i++) {
+	Node()
+	{
+		for (int i = 0; i < 4; i++)
+		{
 			children.resize(4);
 		}
 	}
-	Node(Point k) :point(k) {
+
+	Node(Point k) : point(k)
+	{
 		children.resize(4);
 	}
+
 	friend class QuadTree;
 private:
 	Point point;
 	vector<QuadTree> children;
 };
 
-QuadTree::QuadTree(Point p) : unique_ptr<Node>(new Node(p)) {}
+inline QuadTree::QuadTree(Point p) : unique_ptr<Node>(new Node(p))
+{
+}
 
-QuadTree* QuadTree::Search(Point k) {
+inline QuadTree* QuadTree::Search(Point k)
+{
 	QuadTree* loper = this;
 
-	while (loper->get() != nullptr && (loper->get()->point.x != k.x && loper->get()->point.y != k.y)) {
-		if (k.x <= loper->get()->point.x) {
-			if (k.y <= loper->get()->point.y) {//NW
+	while (loper->get() != nullptr && (loper->get()->point.x != k.x && loper->get()->point.y != k.y))
+	{
+		if (k.x <= loper->get()->point.x)
+		{
+			if (k.y <= loper->get()->point.y)
+			{//NW
 				loper = &(loper->get()->children[0]);
 			}
-			else {//ZW
+			else
+			{//ZW
 				loper = &(loper->get()->children[3]);
 			}
 		}
-		else {
-			if (k.y <= loper->get()->point.y) {//NO
+		else
+		{
+			if (k.y <= loper->get()->point.y)
+			{//NO
 				loper = &(loper->get()->children[1]);
 			}
-			else {//ZO
+			else
+			{//ZO
 				loper = &(loper->get()->children[2]);
 			}
 		}
@@ -74,20 +96,25 @@ QuadTree* QuadTree::Search(Point k) {
 	return loper;
 }
 
-void QuadTree::Add(Point k) {
+inline void QuadTree::Add(Point k)
+{
 	QuadTree* plaats = Search(k);
-	if (plaats->get() == nullptr) {
-		*plaats = move(std::unique_ptr<Node>(new Node(k)));
+	if (plaats->get() == nullptr)
+	{
+		*plaats = move(make_unique<Node>(k));
 	}
 }
 
-int QuadTree::Depth() const
+inline int QuadTree::Depth() const
 {
 	int max_diepte = 0;
-	for (int i = 0; i<4; i++) {
-		if (this->get()->children[i]) {
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->get()->children[i])
+		{
 			int kind_diepte = this->get()->children[i].Depth() + 1;
-			if (kind_diepte > max_diepte) {
+			if (kind_diepte > max_diepte)
+			{
 				max_diepte = kind_diepte;
 			}
 		}
@@ -95,12 +122,15 @@ int QuadTree::Depth() const
 	return max_diepte;
 }
 
-void QuadTree::Write(ostream & os) const
+inline void QuadTree::Write(ostream& os) const
 {
-	if (*this != nullptr) {
+	if (*this != nullptr)
+	{
 		os << this->get()->point.x << " " << this->get()->point.y << endl;
-		for (int i = 0; i<4; i++) {
-			if (this->get()->children[i]) {
+		for (int i = 0; i < 4; i++)
+		{
+			if (this->get()->children[i])
+			{
 				os << "kind: " << i << endl;
 				this->get()->children[i].Write(os);
 			}
